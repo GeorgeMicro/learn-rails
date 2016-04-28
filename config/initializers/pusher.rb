bc9d54ee9d31331e243994666d9282810c2c14c5
Yuxuan Zhao <zhao223@purdue.edu>
2016-04-28 17:07:09 -0400

    get rid of pusher initializer

--- config/initializers/pusher.rb
+++
@@ -1,8 +0,0 @@
-# config/initializers/pusher.rb
-require 'pusher'
-
-Pusher.app_id = '199430'
-Pusher.key = '595ba7cd36689fe387c9'
-Pusher.secret = '115f4021b814b5e1d29f'
-Pusher.logger = Rails.logger
-Pusher.encrypted = true
