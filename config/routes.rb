8c403e957943770127b64adc6f6f2669903c88f3
Yuxuan Zhao <zhao223@purdue.edu>
2016-05-02 12:47:34 -0400

    added chinese form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  scope "(:locale)", locale: /en|nl/ do
+  scope "(:locale)", locale: /en|zh-CN/ do
     resources :visitors, only: [:new, :create]
     resources :contacts, only: [:new, :create]
   end
