636b2e1010ba6727548f5e2492671f24dc5c03de
Yuxuan Zhao <zhao223@purdue.edu>
2016-05-02 02:13:39 -0400

    added translations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  scope "(:locale)", locale: /en|nl/ do
     resources :visitors, only: [:new, :create]
     resources :contacts, only: [:new, :create]
+  end
     root to: 'visitors#new'
+    get '/:locale' => 'visitors#new'
 end
