67a8c7302590d202cd3ea4ab3634c049471e5e8f
Yuxuan Zhao <zhao223@purdue.edu>
2016-05-03 01:08:16 -0400

    fix issue with dropdown menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,6 @@
 //= require turbolinks
 //= require foundation
 //= require_tree .
-$(function() {
-  $(document).foundation();
+$(document).on('ready page:load', function () {
+$(document).foundation();
 });
